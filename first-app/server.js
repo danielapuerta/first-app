fe5cca219575cbb76bca45f4949957e8b5d492ef
danielapuerta <danielapuerta151@gmail.com>
2022-06-25 14:54:40 +0100

    create normalize function

--- first-app/server.js
+++ first-app/server.js
@@ -1,9 +1,29 @@
-
 const http = require('http');
 const app = require('./backend/app');
 const port = process.env.PORT || 3000;
-
 app.set('port',port);
 const server = http.createServer(app);
+
+const normalizePort = val => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+
+
+
+
+
 
 server.listen(port);

4a413106ed256328083f8c25b9bdec2b3d73c9b3
danielapuerta <danielapuerta151@gmail.com>
2022-06-25 13:01:47 +0100

    delete eventEmitter

--- first-app/src/app/posts/post-create/post-create.component.ts
+++ first-app/src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
+import{PostService} from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -13,7 +14,7 @@ we will create an alert message that will show "Post is added successfully". */
 export class PostCreateComponent {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter<Post>();
+
 
   onAddPost(form: NgForm) {
     if (form.invalid) {

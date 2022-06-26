3be24b4fc850e46a84a43a3729e9a9f56270767c
danielapuerta <danielapuerta151@gmail.com>
2022-06-26 11:45:54 +0100

    add subscribe

--- first-app/src/app/posts/posts.service.ts
+++ first-app/src/app/posts/posts.service.ts
@@ -1,18 +1,23 @@
-import{Post} from './post.model';
-import {Injectable} from '@angular/core';
+import { Post } from './post.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
-
-
-export class PostService{
+@Injectable({ providedIn: 'root' })
+export class PostService {
   private posts: Post[] = [];
+  private postUpdated = new Subject<Post[]>();
 
-getPosts(){
-  return this.posts;
-}
+  constructor(private http: HttpClient) {}
+  getPosts() {
+    this.http.get<{message: string, posts: Post[] }>('http://localhost:3000/api/posts')  
+    .subscribe(()=>{   
+        
+    });
+  }
 
-addPost(title: string, content: string){
-  const post: Post={title: title, content: content};
-  this.posts.push(post);
-}
+  addPost(title: string, content: string) {
+    const post: Post = { title: title, content: content };
+    this.posts.push(post);
+  }
 }

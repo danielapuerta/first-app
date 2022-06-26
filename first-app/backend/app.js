4dd9bcf31aafc2c5fb00621d092f5cb8f2190b5f
danielapuerta <danielapuerta151@gmail.com>
2022-06-26 11:20:55 +0100

    jason

--- first-app/backend/app.js
+++ first-app/backend/app.js
@@ -7,9 +7,24 @@ app.use((req, res, next) =>{
 
 app.use('/api/posts', (req, res, next) =>{
   const posts = [
+    {
+      id: '1',
+      title: 'First server-side post',
+      content: 'This is comming from the server'
+    },
+    {
+      id: '2',
+      title: 'Second server-side post',
+      content: 'This is commiing from the server'
+    },
+    {
+      id: '3',
+      title: 'Third server-side post',
+      content: 'This is comming from the server'
+    }
 
   ];
-  res.json();
+  res.json(posts);
 });
 
 module.exports = app;
